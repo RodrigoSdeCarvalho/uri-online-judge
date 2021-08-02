e262fe252465426f4f4738e0b48cb36257cacaed
Rodrigo Santos de Carvalho <rodrigosc2401@gmail.com>
2021-08-02 20:51:48 -0300

    Create 2866.py

---
+++ 2866.py
@@ -0,0 +1,12 @@
+#Problema 1986 do URI
+#Rodrigo Santos de Carvalho
+n=int(input())
+for i in range(0,n):
+    codigo=str(input())
+    codigo_traduzido=list(codigo)
+    for i in range (0, len(codigo)):
+        if codigo[i]==codigo[i].upper():
+            codigo_traduzido.remove(codigo[i])
+    codigo_traduzido.reverse()
+    codigo_traduzido=''.join(codigo_traduzido)
+    print(codigo_traduzido)
